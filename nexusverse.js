fa6e6979a1a316251a3bafdc2a5154163bd5c0dd
sujithachalla97 <168841679+sujithachalla97@users.noreply.github.com>
2024-07-28 16:25:17 +0530

    Update nexusverse.js

--- nexusverse.js
+++ nexusverse.js
@@ -2,6 +2,35 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     document.querySelector('.navbar').classList.toggle('active');
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    let lazyloadImages = document.querySelectorAll("img.lazyload");    
+    let lazyloadThrottleTimeout;
+    
+    function lazyload () {
+        if(lazyloadThrottleTimeout) {
+            clearTimeout(lazyloadThrottleTimeout);
+        }    
+        
+        lazyloadThrottleTimeout = setTimeout(function() {
+            let scrollTop = window.pageYOffset;
+            lazyloadImages.forEach(function(img) {
+                if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazyload');
+                }
+            });
+            if(lazyloadImages.length == 0) { 
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+    }
+    
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+});
 
 
 VanillaTilt.init(document.querySelector(".imgBx"), {
